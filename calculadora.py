147c6a20ebda8577a1fa639295aeb472990f9c6f
Palomah <hues_p@hotmail.com>
2021-05-31 16:22:24 -0300

    Faz subtração

--- calculadora.py
+++ calculadora.py
@@ -1,30 +1,8 @@
-def potencia (a, b):
-  pass
-
-def divisao (a, b):
-  pass
-
-def multiplicacao (a, b):
-  pass
-
 def subtracao (a, b):
-  pass
+  return a-b
 
-def soma (a, b):
-  return a + b
+a = int(input("Digite o primeiro valor para a: "))
+b = int(input ("Digite o segundo valor para b: "))
 
-a = int(input("Digite o primeiro valor"))
-b = int(input ("Digite o segundo valor"))
-
-operacao = input("+: Soma\n-: Subtração\n*: Multiplicação\n/: Divisão\n**: Exponenciação")
-if operacao == '+':
-  resultado = soma(a, b)
-elif operacao == '-':
-  resultado = a - b
-elif operacao == '*':
-  resultado = a * b
-elif operacao == '/':
-  resultado = a // b
-else:
-  resultado = a ** b
+resultado = subtracao(a, b)
 print (resultado)
